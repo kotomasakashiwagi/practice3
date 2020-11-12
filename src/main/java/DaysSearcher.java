400dddb75768ddccbfc2cb58ec6cf8bf64a1232b
kotomasakashiwagi <73920282+kotomasakashiwagi@users.noreply.github.com>
2020-11-12 18:45:41 +0900

    Initial commit

--- src/main/java/DaysSearcher.java
+++
@@ -1,8 +0,0 @@
-public class DaysSearcher {
-    final static int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    //その月が何日あるか
-    public static int searchDaysFromMonth(int i) {
-        return days[i - 1];
-    }
-}
--- src/main/java/IntPrinter.java
+++
@@ -1,6 +0,0 @@
-public class IntPrinter {
-
-    public static void printInt(int i) {
-        System.out.println(i);
-    }
-}
